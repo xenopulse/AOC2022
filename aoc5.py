d94aacb8e840d4d115e2c240e0b250737de93823
xenopulse <59100286+xenopulse@users.noreply.github.com>
2022-12-05 07:00:15 -0800

    Add files via upload

--- aoc5.py
+++ aoc5.py
@@ -1,4 +1,4 @@
-stacks = []
+stacks = {1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
 entries = []
 
 def get_data(filename):
@@ -9,23 +9,44 @@ def get_data(filename):
                 add_to_stacks(line)
             elif line[0] == "m":
                 add_to_entries(line)
+    for stack in stacks:
+        stacks[stack].reverse()
 
 def add_to_stacks(line):
-    pass
+    stacks[1].append(line[1])
+    if len(line) >= 6 and line[5] is not " ":
+        stacks[2].append(line[5])
+    if len(line) >= 10 and line[9] is not " ":
+        stacks[3].append(line[9])
+    if len(line) >= 14 and line[13] is not " ":
+        stacks[4].append(line[13])
+    if len(line) >= 18 and line[17] is not " ":
+        stacks[5].append(line[17])
+    if len(line) >= 22 and line[21] is not " ":
+        stacks[6].append(line[21])
+    if len(line) >= 26 and line[25] is not " ":
+        stacks[7].append(line[25])
+    if len(line) >= 30 and line[29] is not " ":
+        stacks[8].append(line[29])
+    if len(line) >= 34 and line[33] is not " ":
+        stacks[9].append(line[33])
+
 
 def add_to_entries(line):
     parts = line.split(" ")
-    entries.append(split[0], etc)
-    pass
+    entries.append([int(parts[1]), int(parts[3]), int(parts[5][0])])
 
 get_data("aoc5.txt")
 
-def move_crate(entry):
-    pass
+print(stacks)
+print(entries)
 
-final = ""
+def move_crate(entry):
+    for i in range(0,entry[0]):
+        moving = stacks[entry[1]].pop()
+        stacks[entry[2]].append(moving)
 
 for entry in entries:
     move_crate(entry)
 
-# print first of each row
+print(stacks)
